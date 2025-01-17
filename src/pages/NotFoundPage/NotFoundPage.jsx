fe5a64d81951234f08e3fe509908a5c44046ec36
Tanya Yenakieva <yastigma@gmail.com>
2025-01-18 01:05:18 +0200

    not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,11 @@
+
+import CSS from "./NotFoundPage.module.css"
 export default function NotFoundPage() {
     return(
-        <div>
-            Page not found
-        </div>
+             <main className={CSS.wrapper}>
+<h2 className={CSS.title}>Page not found</h2>
+<p className={CSS.text}>Please return to home page</p>
+ </main>
+
     )
 };

cb3b1122e5b1f28d8a1bbf86f0271dd2bc759dc0
Tanya Yenakieva <yastigma@gmail.com>
2025-01-19 14:20:06 +0200

    Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,48 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCamperID } from '../../redux/campers/operations';
+import { selectCamperId } from '../../redux/campers/selectors';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import StarGoldIcon from '../../icon/star-gold.svg?react';
+import StarGrayIcon from '../../icon/star.svg?react';
+import CSS from './Reviews.module.css';
 export default function Reviews() {
-    return(
-        <div>3333333</div>
-    )
-};
+  const dispatch = useDispatch();
+  const camperData = useSelector(selectCamperId);
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchCamperID(id));
+    }
+  }, [dispatch, id]);
+
+  if (!camperData) {
+    return <div>Loading...</div>; // Или ваш компонент Loader
+  }
+  const { reviews } = camperData;
+
+  function renderStars(reviewer_rating) {
+    const totalStars = 5;
+    return Array.from({ length: totalStars }, (_, index) => {
+      return index < reviewer_rating ? <StarGoldIcon key={index} /> : <StarGrayIcon key={index} />;
+    });
+  }
+
+  return (
+    <ul className={CSS.wrapper}>
+      {reviews.map(({ id, comment, reviewer_name, reviewer_rating }) => (
+        <li key={id} className={CSS.commentBox}>
+          <div className={CSS.person}>
+            <span className={CSS.avatar}>{reviewer_name.charAt(0).toUpperCase()}</span>
+            <div>
+              <p className={CSS.name}>{reviewer_name}</p>
+              <div>{renderStars(reviewer_rating)}</div>
+            </div>
+          </div>
+          <div className={CSS.comment}>{comment}</div>
+        </li>
+      ))}
+    </ul>
+  );
+}
